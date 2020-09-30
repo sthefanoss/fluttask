bf2f720a559d223cb79a82889d6bd80da6c13857
Sthefano Schiavon <sthefanoss@gmail.com>
2020-09-30 20:25:57 -0300

    add login and registration MVPs

--- lib/views/pages/login_form_page.dart
+++ lib/views/pages/login_form_page.dart
@@ -1,4 +1,6 @@
+import 'package:fluttask/routing/route_names.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class LoginFormPage extends StatefulWidget {
   @override
@@ -6,8 +8,63 @@ class LoginFormPage extends StatefulWidget {
 }
 
 class _LoginFormPageState extends State<LoginFormPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Form(
+        key: formKey,
+        child: SafeArea(
+          child: Container(
+            padding:  const  EdgeInsets.all(32),
+            alignment: Alignment.center,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                TextFormField(
+                  controller: emailController,
+                  decoration: InputDecoration(labelText: 'E-mail'),
+                ),
+                const SizedBox(height: 8,),
+                TextFormField(
+                  controller: passwordController,
+                  decoration: InputDecoration(labelText: 'Senha'),
+                ),
+               const SizedBox(height: 8,),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    RaisedButton(
+                      child: Text('Registrar'),
+                      onPressed: _navigateToRegistrationPage,
+                    ),
+                    RaisedButton(
+                      child: Text('Entrar'),
+                      onPressed: _login,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
+
+  void _navigateToRegistrationPage() {
+    Get.toNamed(RouteNames.registrationForm);
+  }
+
+  Future<void> _login() async {}
 }
--- lib/views/pages/registration_form_page.dart
+++ lib/views/pages/registration_form_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class RegistrationFormPage extends StatefulWidget {
   @override
@@ -6,8 +7,131 @@ class RegistrationFormPage extends StatefulWidget {
 }
 
 class _RegistrationFormPageState extends State<RegistrationFormPage> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  final confirmPasswordController = TextEditingController();
+  final nameController = TextEditingController();
+  final cpfController = TextEditingController();
+  final cepController = TextEditingController();
+  final locationController = TextEditingController();
+  final numberController = TextEditingController();
+  final dateOfBirthController = TextEditingController();
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    confirmPasswordController.dispose();
+    nameController.dispose();
+    cpfController.dispose();
+    cepController.dispose();
+    locationController.dispose();
+    numberController.dispose();
+    dateOfBirthController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Registro de Usuário"),
+      ),
+      body: SafeArea(
+        child: Form(
+          key: formKey,
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView(
+                  padding: const EdgeInsets.all(32),
+                  children: [
+                    Text(
+                      "Dados Pessoais",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    TextFormField(
+                      controller: nameController,
+                      decoration: InputDecoration(labelText: 'Nome *'),
+                    ),
+                    TextFormField(
+                      controller: cpfController,
+                      decoration: InputDecoration(labelText: 'CPF'),
+                    ),
+                    TextFormField(
+                      controller: dateOfBirthController,
+                      decoration:
+                          InputDecoration(labelText: 'Data de Nascimento *'),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Text(
+                      "Credenciais",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    TextFormField(
+                      controller: emailController,
+                      decoration: InputDecoration(labelText: 'E-mail *'),
+                    ),
+                    TextFormField(
+                      controller: passwordController,
+                      decoration: InputDecoration(labelText: 'Senha *'),
+                    ),
+                    TextFormField(
+                      controller: confirmPasswordController,
+                      decoration:
+                          InputDecoration(labelText: 'Confirmar Senha *'),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Text(
+                      "Endereço",
+                      style: Theme.of(context).textTheme.headline6,
+                    ),
+                    TextFormField(
+                      controller: cepController,
+                      decoration: InputDecoration(labelText: 'CEP'),
+                    ),
+                    TextFormField(
+                      controller: locationController,
+                      decoration: InputDecoration(labelText: 'Endereço'),
+                    ),
+                    TextFormField(
+                      controller: numberController,
+                      decoration: InputDecoration(labelText: 'Número'),
+                    ),
+                    const SizedBox(
+                      height: 32,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        RaisedButton(
+                          child: Text('Voltar'),
+                          onPressed: _navigateBack,
+                        ),
+                        RaisedButton(
+                          child: Text('Registrar'),
+                          onPressed: _accept,
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
+
+  void _navigateBack() {
+    Get.back();
+  }
+
+  Future<void> _accept() async {}
 }
