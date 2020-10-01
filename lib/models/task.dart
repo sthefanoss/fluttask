fdec010dba306463a216cc938a10d43028c2fbf7
Sthefano Schiavon <sthefanoss@gmail.com>
2020-10-01 00:50:29 -0300

    update task model with copyWith

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,19 +1,39 @@
 class Task {
-  final int id;
+  final String id;
   final int userId;
   final String name;
   final String description;
-  final DateTime creationDate;
-  final DateTime conclusionDate;
+  final DateTime dateOfDelivery;
+  final DateTime dateOfConclusion;
   final bool isConcluded;
 
   const Task({
     this.id,
     this.userId,
     this.name,
-    this.description,
-    this.creationDate,
-    this.conclusionDate,
-    this.isConcluded,
+    this.description = '',
+    this.dateOfDelivery,
+    this.dateOfConclusion,
+    this.isConcluded = false,
   });
+
+  Task copyWith({
+    String id,
+    int userId,
+    String name,
+    String description,
+    DateTime dateOfDelivery,
+    DateTime dateOfConclusion,
+    bool isConcluded,
+  }) {
+    return Task(
+      id: id ?? this.id,
+      userId: userId ?? this.userId,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      dateOfDelivery: dateOfDelivery ?? this.dateOfDelivery,
+      dateOfConclusion: dateOfConclusion ?? this.dateOfConclusion,
+      isConcluded: isConcluded ?? this.isConcluded,
+    );
+  }
 }
