166c1f7e9201afc6ead5d1b73931353b3f10f09f
Sthefano Schiavon <sthefanoss@gmail.com>
2020-10-01 00:51:56 -0300

    implement tasks controller

--- lib/constrollers/tasks_controller.dart
+++ lib/constrollers/tasks_controller.dart
@@ -4,23 +4,49 @@ import '../models/task.dart';
 class TasksController extends GetxController {
   final tasks = RxList<Task>([]);
 
-  Future<void> init() async {
+  Future<bool> init() async {
     //TODO
   }
 
-  Future<void> createTask(Task task) async {
-    //TODO
+  Future<bool> createOrEditTask(Task task) async {
+    final int taskIndex = _findTaskIndexById(task.id);
+
+    if (taskIndex == -1) {
+      tasks.add(task);
+    } else {
+      tasks[taskIndex] = tasks[taskIndex].copyWith(
+          name: task.name,
+          description: task.description,
+          dateOfConclusion: task.dateOfConclusion,
+          dateOfDelivery: task.dateOfDelivery);
+    }
+
+    return true;
   }
 
-  Future<void> excludeTask(int id) async {
-    //TODO
+  Future<bool> deleteTask(String id) async {
+    final int taskIndex = _findTaskIndexById(id);
+
+    tasks.removeAt(taskIndex);
+
+    return true;
   }
 
-  Future<void> updateTask(Task updatedTask) async {
-    //TODO
+  Future<bool> concludeTask(String id) async {
+    final int taskIndex = _findTaskIndexById(id);
+    print(taskIndex);
+
+    if (tasks[taskIndex].isConcluded) return false;
+
+    tasks[taskIndex] = tasks[taskIndex].copyWith(
+      isConcluded: true,
+      dateOfConclusion: tasks[taskIndex].dateOfConclusion ?? DateTime.now(),
+    );
+
+    return true;
   }
 
-  Future<void> concludeTask(int id) async {
-    //TODO
+  int _findTaskIndexById(String id) {
+    return tasks.indexWhere((task) => task.id == id);
   }
 }
